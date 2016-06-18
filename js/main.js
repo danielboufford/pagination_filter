f70915839defc641060a758732585e00df0dd19d
daniel boufford <daniel.boufford@gmail.com>
2016-06-18 18:13:29 -0400

    added search feature

--- js/main.js
+++ js/main.js
@@ -1,59 +1,96 @@
 (function(){
   "use strict"
 
+  var SEARCH_TEMPLATE =  '<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>',
+      NAVIGATION_CONTAINER = '<div class="pagination"><ul></ul></div>';
+
   var $students = $(".student-item"),
-      $studentNames = $(".student-details h3"),
-      $studentEmails = $(".email");
+      $pageBody = $(".page");
 
-  //append search feature, navigation links, and show only students 1-10 
+  init();
 
-  function addHTML() {
+  function filterResults(){
+    var searchInput = $(".student-search input").val().toLowerCase();
+    
+    var matchingStudents = $students.filter(match);
 
-    var searchFeature =  '<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>';   
-    $(".page-header").append(searchFeature);
+    swapOutPage($students, matchingStudents);
 
-    var navigationContainer = '<div class="pagination"><ul></ul></div>'
-    $(".page").append(navigationContainer);
+    function matchName(item) {
+      var nameOfStudent = $(item).find('.student-details h3')[0].innerHTML;
+      return nameOfStudent.indexOf(searchInput) !== -1      
+    }
 
-    students.hide().slice(0,10).show();
+    function matchEmail(item) {
+      var emailOfStudent = $(item).find('.email')[0].innerHTML;
+      return emailOfStudent.indexOf(searchInput) !== -1   
+    }
 
-    var pagesNeeded = Math.ceil(students.length / 10);
-    for(var i = 0; i < pagesNeeded; i++) {
-      var li = '<li><a href="#">' + (i + 1) + '</a></li>'
-      $(".pagination ul").append(li);
+    function match() {
+      return matchName(this) || matchEmail(this);
+    }
+
+    
+  }
+
+  function switchPage(){
+    //variable that equals link number
+    var linkNumber = $(this).html(),
+        pageStart = (linkNumber * 10) - 10,
+        pageEnd = linkNumber * 10,
+        $activeLink = $(this);
+
+    deselectAllPagination();
+    selectActivePage();
+    swapOutPage($students, $students.slice(pageStart,pageEnd));
+
+    function deselectAllPagination() {
+      var $paginationLinks =  $(".pagination a");
+      $paginationLinks.removeClass("active");
+    }
+
+    function selectActivePage() {
+      $activeLink.addClass("active");
     }
   }
-  addHTML();
 
-  //when link is clicked, add "active" classand remove for all other links
+  function swapOutPage(allStudents, studentsToDisplay) {
+    allStudents.hide();
+    studentsToDisplay.show();
+  }
 
-  $(".pagination a").click(function(){
-    $("a").each(function(){
-      $(this).removeClass("active");
-    });
-  $(this).addClass("active");
+  function init() {
+    addSearch();
+    addPagination();
+    displayStudents();
+    registerEvents();
 
-  //variable that equals link number
-    var linkNumber = $(this).html();
+    function addSearch() {
+      $(".page-header").append(SEARCH_TEMPLATE);
+    }
 
-  //display the results corresponding with each link number
+    function addPagination() {
+      var pagesNeeded = Math.ceil($students.length / 10);
 
-    students.hide().slice((linkNumber*10)-10,(linkNumber * 10)).show();
+      $pageBody.append(NAVIGATION_CONTAINER);
 
-  });
-
-  //search function
-
-  $(".student-search button").click(function(){
-    var searchInput = $(".student-search input").val().toLowerCase();;
-    students.hide();
-    for(i = 0; i < students.length; i++) {
-
-      if(studentArray[i].indexOf(searchInput) !== -1) {
-        studentArray[i].show();
+      for(var i = 0; i < pagesNeeded; i++) {
+        // TODO make first link active
+        var li = '<li><a href="#">' + (i + 1) + '</a></li>'
+        $(".pagination ul").append(li);
       }
     }
 
-  });
+    function displayStudents() {
+      $students.hide().slice(0,10).show();
+    }
 
+    function registerEvents() {
+      var $paginationLinks =  $(".pagination a"),
+          $searchButton =   $(".student-search button");
+
+      $paginationLinks.click(switchPage);
+      $searchButton.click(filterResults);
+    }
+  }
 }())
